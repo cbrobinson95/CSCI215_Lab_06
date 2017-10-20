a65047c5331a47b6f2bddf76939d5980e1f6e593
cbrobinson95 <31280105+cbrobinson95@users.noreply.github.com>
2017-10-20 09:16:09 -0400

    My Final Commit

--- action.js
+++ action.js
@@ -1,15 +1,45 @@
-function checkEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+function checkForBracket() {
+    var regExp = new RegExp('<');
+    var name = document.getElementById('name').value;
 
+    if (regExp.test(name)) {
+        console.log('contains illegal characters');
+        document.getElementById('name').style.backgroundColor='red';
+    } else {
+        document.getElementById('name').style.backgroundColor = '';
+    }
 }
 
 function checkAge() {
     var regEx = new RegExp('\w');
-    
-    if() {
-       
+    var age = document.getElementById('age').value;
+
+    if(regEx.test(age)) {
+        console.log('contains illegal characters' + document.getElementById('age').value);
+        document.getElementById('age').style.backgroundColor='red';
     } else {
-       
+        document.getElementById('age').style.backgroundColor='';
     }
+}
+
+function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function myAjaxCall() {
+    alert("I made my AJAX Call");
+}
+
+function validate() {
+    if(checkEmail(document.getElementById('email').value)) {
+        myAjaxCall();
+
+        var inputs = document.getElementsByClassName('input');
+        for(var i=0; i<inputs.length; i++) {
+            inputs[i].value = '';
+        }
+        } else {
+            alert('I failed to validate. Check information');
+        }
 }
